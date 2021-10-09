735189760ea5a2216400f375937ed710b820eee0
Dominik B <byrtus.dominik@gmial.com>
2021-10-09 10:50:59 +0200

    Initial commit

---
+++ src/rasterize/TrivialLineRasterizer.java
@@ -0,0 +1,43 @@
+package rasterize;
+
+public class TrivialLineRasterizer extends LineRasterizer {
+
+    public TrivialLineRasterizer(Raster raster) {
+        super(raster);
+    }
+
+    @Override
+    public void rasterize(int x1, int y1, int x2, int y2, int color) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        float k =  dy/ (float) dx;
+        float q = y1 - k * x1;
+        if(Math.abs(dy) < Math.abs(dx)){
+            if (x2<x1) {
+                for (int x = x2 ; x <=x1 ; x ++) {
+                    float y = k * x +q;
+                    raster.setPixel(x, Math.round(y),color);
+                }
+            }else {
+                for (int x = x1; x <= x2; x++) {
+                    float y = k * x + q;
+                    raster.setPixel(x, Math.round(y), color);
+                }
+            }
+        } else {
+            if (y2<y1) {
+                for (int y = y2 ; y <=y1 ; y ++) {
+                    float x = (y-q)/k;
+                    raster.setPixel(Math.round(x),y,color);
+                }
+            }else {
+                for (int y = y1 ; y <=y2 ; y ++) {
+                    float x = (y-q)/k;
+                    raster.setPixel(Math.round(x), y, color);
+                }
+            }
+
+        }
+        // else jiná řídící osa
+    }
+}
